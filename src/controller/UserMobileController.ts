122fea9feee7fca36c61fdd995ca466e12ec4c60
fischerjoao <joao-fischer@hotmail.com>
2025-06-01 00:44:30 -0300

    :lock: feat: Implement password recovery and change functionalities with email notifications

--- src/controller/UserMobileController.ts
+++ src/controller/UserMobileController.ts
@@ -5,6 +5,8 @@ import jwt from "jsonwebtoken";
 import { UserSchema } from "../validations/userSchema";
 const prisma = new PrismaClient();
 const JWT_SECRET = process.env.JWT_SECRET;
+const emailApp = process.env.GOOGLE_EMAIL;
+const appPassword = process.env.GOOGLE_APP_PASSWORD;
 
 export class UserMobileController {
   getAllUserMobile = async (req: Request, res: Response) => {
@@ -58,7 +60,6 @@ export class UserMobileController {
   };
   loginUserMobile = async (req: Request, res: Response) => {
     try {
-      // Validação dos dados de entrada
       await UserSchema.loginMobile.validate(req.body);
 
       const { email, password } = req.body; // Verifica se o usuário existe
@@ -98,6 +99,129 @@ export class UserMobileController {
       } else {
         res.status(500).json({ message: "Erro ao fazer login" });
       }
+    }
+  };
+
+  forgotPasswordEmail = async (req: Request, res: Response) => {
+    try {
+      await UserSchema.forgotPassword.validate(req.body);
+
+      const { email } = req.body;
+      const user = await prisma.user.findUnique({ where: { email } });
+
+      if (!user) {
+        return res.status(400).json({ message: "Email inválido" });
+      }
+
+      const resetToken = crypto.randomUUID();
+      const resetTokenExpiry = new Date(Date.now() + 3600000);
+
+      await prisma.user.update({
+        where: { email },
+        data: { resetToken, resetTokenExpiry },
+      });
+
+      const resetLink = `http://localhost:4000/reset-password-mobile.html?token=${resetToken}`;
+
+      const nodemailer = require("nodemailer");
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: emailApp,
+          pass: appPassword,
+        },
+      });
+
+      const mailOptions = {
+        from: emailApp,
+        to: user.email,
+        subject: "Recuperação de senha",
+        html: `
+        Olá, ${user.name},<br><br>
+        Você solicitou a recuperação de senha.<br>
+        Clique no link abaixo para redefinir sua senha:<br>
+        <a href="${resetLink}">${resetLink}</a><br><br>
+        Este link é válido por 1 hora.<br><br>
+        Atenciosamente,<br>
+        Equipe Capitech
+      `,
+      };
+
+      await transporter.sendMail(mailOptions);
+
+      return res.status(200).json({
+        message: "Email de recuperação enviado com sucesso",
+      });
+    } catch (error: any) {
+      console.error("Erro no envio de email:", error);
+      return res.status(500).json({
+        message: "Erro ao enviar email de recuperação",
+        error: error.message || error,
+      });
+    }
+  };
+
+  resetPassword = async (req: Request, res: Response) => {
+    await UserSchema.resetPassword.validate(req.body);
+    const { token, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        resetToken: token,
+        resetTokenExpiry: { gte: new Date() },
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "Token inválido ou expirado" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        password: hashedPassword,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+    res
+      .status(200)
+      .json({ sucess: true, message: "Senha redefinida com sucesso" });
+  };
+
+  changePassword = async (req: Request, res: Response) => {
+    try {
+      await UserSchema.changePassword.validate(req.body);
+      const { password, newPassword } = req.body;
+
+      const userId = (req as any).userId;
+      if (!userId) {
+        return res.status(401).json({ message: "Usuário não autenticado" });
+      }
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+      if (!user) {
+        return res.status(404).json({ message: "Usuário não encontrado" });
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: "Senha atual inválida" });
+      }
+      const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+      await prisma.user.update({
+        where: { id: userId },
+        data: {
+          password: hashedNewPassword,
+        },
+      });
+      return res.status(200).json({ message: "Senha alterada com sucesso" });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message });
+      }
+      return res.status(500).json({ message: "Erro ao alterar senha" });
     }
   };
 }
